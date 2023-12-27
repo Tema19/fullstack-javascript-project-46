5213453747d46324a129e563f69284e7d4419799
Tema <artemiyd90@gmail.com>
2023-12-27 16:37:23 +0100

    Added fixtures. Setup first test.

---
+++ __tests__/gendiffFunc.test.js
@@ -0,0 +1,18 @@
+import gendiff from '../src/gendiffFunc.js';
+import fs from 'fs/promises';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
+const result = await readFile('result.txt')
+
+  test('gendiff', () => {
+    gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), (_error, currentResult) => {
+    expect(currentResult).toEqual(result)
+  });
+});
--- src/gendiffFunc.js
+++ src/gendiffFunc.js
@@ -4,31 +4,75 @@ import path from 'path';
 const readJsonFile = (filepath, cb) => {
   fs.readFile(path.resolve(filepath), 'utf-8', (err, data) => {
     if (err) {
-      console.error(err);
+      Error(err);
       return;
     }
     cb(err, JSON.parse(data));
   });
 };
 
-export default function gendiff(filepath1, filepath2) {
-  readJsonFile(filepath1, (error, data1) => {
-    readJsonFile(filepath2, (error, data2) => {
+function compareFunc(a, b) {
+  if (a.propKey > b.propKey) { return 1; }
+  if (b.propKey > a.propKey) { return -1; }
+  return 0;
+}
+
+// export default function gendiff(filepath1, filepath2) {
+//   readJsonFile(filepath1, (_error1, data1) => {
+//     readJsonFile(filepath2, (_error2, data2) => {
+//       const result = [];
+//       Object.entries(data1).forEach((entry1) => {
+//         const [key1, value1] = entry1;
+//         if (!Object.keys(data2).includes(key1)) {
+//           result.push({ propKey: key1, propValue: value1, propStatus: '-' });
+//         }
+//       });
+//       Object.entries(data2).forEach((entry2) => {
+//         const [key2, value2] = entry2;
+//         if (!Object.keys(data1).includes(key2)) {
+//           result.push({ propKey: key2, propValue: value2, propStatus: '+' });
+//         }
+//       });
+//       Object.entries(data1).forEach((entry1) => {
+//         const [key1, value1] = entry1;
+//         Object.entries(data2).forEach((entry2) => {
+//           const [key2, value2] = entry2;
+//           if ((key1 === key2) && (value1 === value2)) {
+//             result.push({ propKey: key1, propValue: value1, propStatus: ' ' });
+//           }
+//           if ((key1 === key2) && (value1 !== value2)) {
+//             result.push({ propKey: key1, propValue: value1, propStatus: '-' });
+//             result.push({ propKey: key1, propValue: value2, propStatus: '+' });
+//           }
+//         });
+//       });
+//       const sortedResult = result.sort(compareFunc)
+//         .map((prop) => `   ${prop.propStatus} ${prop.propKey} : ${prop.propValue}`);
+//       const printedResult = `{\n${sortedResult.join('\r\n')}\n}`;
+//     });
+//   });
+// }
+
+const gendiff = (filepath1, filepath2, callback) => {
+  readJsonFile(filepath1, (_error1, data1) => {
+    readJsonFile(filepath2, (_error2, data2) => {
       const result = [];
-      const entries1 = Object.entries(data1);
-      const entries2 = Object.entries(data2);
-      for (const [key1, value1] of entries1) {
+      Object.entries(data1).forEach((entry1) => {
+        const [key1, value1] = entry1;
         if (!Object.keys(data2).includes(key1)) {
           result.push({ propKey: key1, propValue: value1, propStatus: '-' });
         }
-      }
-      for (const [key2, value2] of entries2) {
+      });
+      Object.entries(data2).forEach((entry2) => {
+        const [key2, value2] = entry2;
         if (!Object.keys(data1).includes(key2)) {
           result.push({ propKey: key2, propValue: value2, propStatus: '+' });
         }
-      }
-      for (const [key1, value1] of entries1) {
-        for (const [key2, value2] of entries2) {
+      });
+      Object.entries(data1).forEach((entry1) => {
+        const [key1, value1] = entry1;
+        Object.entries(data2).forEach((entry2) => {
+          const [key2, value2] = entry2;
           if ((key1 === key2) && (value1 === value2)) {
             result.push({ propKey: key1, propValue: value1, propStatus: ' ' });
           }
@@ -36,14 +80,14 @@ export default function gendiff(filepath1, filepath2) {
             result.push({ propKey: key1, propValue: value1, propStatus: '-' });
             result.push({ propKey: key1, propValue: value2, propStatus: '+' });
           }
-        }
-      }
-      const sortedResult = result.sort((a, b) => {
-        ((a.propKey > b.propKey) ? 1 : ((b.propKey > a.propKey) ? -1 : 0))
-      })
+        });
+      });
+      const sortedResult = result.sort(compareFunc)
         .map((prop) => `   ${prop.propStatus} ${prop.propKey} : ${prop.propValue}`);
-      const printedResult = '{\n' + `${sortedResult.join('\r\n')}` + '\n}';
-      console.log(printedResult);
+      const printedResult = `{\n${sortedResult.join('\r\n')}\n}`;
+      callback(null, printedResult);
     });
   });
-}
+};
+
+export default gendiff;
