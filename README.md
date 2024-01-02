3a34ab071aed872da9b05e1a395da209c363bb20
Tema <artemiyd90@gmail.com>
2024-01-02 20:49:52 +0100

    Eslint config file changies. Badge added to README

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Tema19/fullstack-javascript-project-46/workflows/hexlet-check/badge.svg)](https://github.com/Tema19/fullstack-javascript-project-46/actions)
 
+[![Actions Status](https://github.com/Tema19/fullstack-javascript-project-46/workflows/project-tests/badge.svg)](https://github.com/Tema19/fullstack-javascript-project-46/actions)
+
+<a href="https://codeclimate.com/github/Tema19/fullstack-javascript-project-46/maintainability"><img src="https://api.codeclimate.com/v1/badges/83ac941e624eacf49efa/maintainability" /></a>
+
+<a href="https://codeclimate.com/github/Tema19/fullstack-javascript-project-46/test_coverage"><img src="https://api.codeclimate.com/v1/badges/83ac941e624eacf49efa/test_coverage" /></a>
+
 <a href="https://asciinema.org/a/gvRRWimcamEpA8opTEqthEYeX" target="_blank"><img src="https://asciinema.org/a/gvRRWimcamEpA8opTEqthEYeX.svg" /></a>
--- __tests__/gendiffFunc.test.js
+++ __tests__/gendiffFunc.test.js
@@ -1,18 +1,18 @@
-import gendiff from '../src/gendiffFunc.js';
 import fs from 'fs/promises';
-import path from 'path';
-import { dirname } from 'path';
+import path, { dirname } from 'path';
+
 import { fileURLToPath } from 'url';
+import gendiff from '../src/gendiffFunc.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
-const result = await readFile('result.txt')
+const result = await readFile('result.txt');
 
-  test('gendiff', () => {
-    gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), (_error, currentResult) => {
-    expect(currentResult).toEqual(result)
+test('gendiff', () => {
+  gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), (_error, currentResult) => {
+    expect(currentResult).toEqual(result);
   });
 });
